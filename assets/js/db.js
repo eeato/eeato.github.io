e2814943eeb5d14870aedc76ed66a94db29eca34
eeato <137734862+eeato@users.noreply.github.com>
2023-09-02 22:41:21 +0200

    Update db.js

--- assets/js/db.js
+++ assets/js/db.js
@@ -64,55 +64,6 @@ try {
 
 
 
-function uploadFile(childref, file, oncomplete, onerror) {
-  let rtnURL;
-  var uploadTask = childref.put(file);
-
-  // Register three observers:
-  // 1. 'state_changed' observer, called any time the state changes
-  // 2. Error observer, called on failure
-  // 3. Completion observer, called on successful completion
-  uploadTask.on(
-    "state_changed",
-    (snapshot) => {
-      // Observe state change events such as progress, pause, and resume
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log("Upload is " + progress + "% done");
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log("Upload is paused");
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log("Upload is running");
-          break;
-      }
-    },
-    (error) => {
-      // Handle unsuccessful uploads
-      onerror(error)
-    },
-    () => {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-        console.log("File available at", downloadURL);
-        rtnURL = downloadURL;
-        oncomplete(downloadURL);
-      });
-    }
-  );
-  return rtnURL;
-}
-function deleteFile(childref, oncomplete, onerror) {
-  // Delete the file
-  childref.delete().then(() => {
-    oncomplete();
-  }).catch((error) => {
-    onerror(error);
-  });
-}
-
 
 document.querySelector('body > header > div > div.left > a').addEventListener('click', event => {
     firebase.auth().onAuthStateChanged((userr) => {
